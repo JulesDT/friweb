7b94a22127d7ee812fa7a209a25a569f07fda588
JulesDT <jules.denardou@datadoghq.com>
2017-12-04 10:34:32 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import re
+
+with open('cacm.all') as f:
+    full_document = f.read()
+    document_list = re.split('^\.I ', full_document, flags=re.MULTILINE)
+    for document in document_list[1:2]:
+        doc_parts = re.split('^\.', document, flags=re.MULTILINE)
+        print doc_parts[3]
