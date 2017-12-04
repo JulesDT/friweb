500a300f9257df3aa35c685a45ea733913925b61
Ghislain Jeanneau <ghislain@messagera.com>
2017-12-04 10:45:43 +0100

    Added document class

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import re
 
+class Document:
+    def __init__(self, str):
+        # TODO jules
+        # parser le string et creer l objet
+        pass
+
+    def __init__(self, i, t, w, k):
+        self.id = i
+        self.title = t
+        self.summary = w
+        self.keywords = k
+
 with open('cacm.all') as f:
     full_document = f.read()
     document_list = re.split('^\.I ', full_document, flags=re.MULTILINE)
