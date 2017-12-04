613aaf1798190649ca7403443405c22d83109412
JulesDT <jules.denardou@datadoghq.com>
2017-12-04 10:35:29 +0100

    switch to python 3.6

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ with open('cacm.all') as f:
     document_list = re.split('^\.I ', full_document, flags=re.MULTILINE)
     for document in document_list[1:2]:
         doc_parts = re.split('^\.', document, flags=re.MULTILINE)
-        print doc_parts[3]
+        print(doc_parts[3])
