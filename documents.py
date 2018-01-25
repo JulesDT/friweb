5df23e5704d45c3b4ce84f0d9c01dde7b2f7dd94
JulesDT <jules.denardou@datadoghq.com>
2018-01-25 15:17:09 +0100

    parsing done + split files

---
+++ documents.py
@@ -0,0 +1,172 @@
+import re
+import glob
+
+
+class SparseWordVector:
+    def __init__(self, v):
+        self.v = v
+        self.resetCache()
+
+    def resetCache(self):
+        self.norm = -1
+        self.cosSilimarity = 2
+
+    def norm(self):
+        if self.norm == -1:
+            self.norm = sum([v * v for k, v in self.v.items()])
+        return self.norm
+
+    def cosSilimarity(self, other):
+        if self.cosSilimarity == 2:
+            v1 = self.v
+            v2 = other.v
+            v1_dims = set(v1.keys)
+            v2_dims = set(v2.keys)
+            common_dims = v1_dims.intersection(v2_dims)
+            num = sum([v1[dim] * v2[dim] for dim in common_dims])
+            self.cosSilimarity = num / (self.norm() * other.norm())
+        return self.cosSilimarity
+
+
+class DocumentTokenizer:
+    def __init__(self, stop_list):
+        self.stop_list = stop_list
+
+    def tokenize(self, s, normalizer):
+        reg = re.compile(r"[a-zA-Z]+")
+        for token in reg.findall(s):
+            normalized_token = token
+            if self.stop_list.valid(normalized_token):
+                yield normalized_token
+
+
+class DocumentNormalizer:
+    @staticmethod
+    def normalize(token):
+        return token.lower()
+
+
+class InvertedIndex:
+    def __init__(self):
+        self.inverted_index = {}
+        self.base_dict = {}
+
+    def __str__(self):
+        res = ""
+        for (key, val) in self.inverted_index.items():
+            res += key + str(val) + "\n"
+        return res
+
+    def filter(self, pattern):
+        copy = InvertedIndex()
+        copy.inverted_index = {key: val for (key, val) in self.inverted_index.items() if re.match(pattern, key)}
+        return copy
+
+    def register(self, token, documentId):
+        if token in self.inverted_index:
+            self.inverted_index[token].add(documentId)
+        else:
+            self.inverted_index[token] = set([documentId])
+
+    def merge(self, inv_index):
+        for token in inv_index.inverted_index.keys():
+            if token in self.inverted_index:
+                self.inverted_index[token].update(inv_index.inverted_index[token])
+            else:
+                self.inverted_index[token] = inv_index.inverted_index[token]
+
+    def build_base_vector(self):
+        self.base_dict = {k: v for v, k in enumerate(self.inverted_index.keys())}
+        print(self.base_dict)
+
+
+class Document:
+    def __init__(self):
+        self.fields_to_tokenize = []
+        self.id = ""
+
+    def tokenize(self, tokenizer, normalizer, inverted_index):
+        for field in self.fields_to_tokenize:
+            setattr(self, field + '_tokens', [word for word in tokenizer.tokenize(getattr(self, field), normalizer)])
+            for token in getattr(self, field + '_tokens'):
+                inverted_index.register(token, self.id)
+
+
+class CASMBlock:
+    def __init__(self, path):
+        self.path = path
+
+    def get_next_block(self):
+        doc_list = set()
+        with open(self.path) as f:
+            full_document = f.read()
+            document_list = re.split('^\.I ', full_document, flags=re.MULTILINE)
+            for document in document_list:
+                doc = CACMDocument.from_string(document)
+                doc_list.add(doc)
+            yield doc_list
+
+
+class CS276Block:
+    def __init__(self, path):
+        self.path = path
+
+    def get_next_block(self):
+        i = 0
+        for filename in glob.glob(self.path):
+            doc_list = set()
+            print('Reading ' + filename)
+            for documentFileName in glob.glob(filename + '/*'):
+                with open(documentFileName) as f:
+                    document = f.read()
+                    doc = CS276Document(document, i)
+                    doc_list.add(doc)
+                    i += 1
+            yield doc_list
+
+
+class CACMDocument(Document):
+    def __init__(self, i, t, w, k):
+        Document.__init__(self)
+        self.id = i
+        self.title = t
+        self.summary = w
+        self.keywords = k
+
+        self.fields_to_tokenize = ["title", "summary", "keywords"]
+
+    @classmethod
+    def from_string(self, document):
+        doc_parts = re.split('^\.', document, flags=re.MULTILINE)
+        summary = ""
+        keywords = ""
+        title = ""
+        identifier = 0
+        if len(doc_parts) > 0 and doc_parts[0] != '':
+            identifier = int(doc_parts[0])
+            for element in doc_parts:
+                if element.startswith('T'):
+                    title = element.split('\n')[1]
+                elif element.startswith('W'):
+                    summary = element.split('\n')[1]
+                elif element.startswith('K'):
+                    keywords = element.split('\n')[1]
+        return CACMDocument(identifier, title, summary, keywords)
+
+
+class CS276Document(Document):
+    def __init__(self, content, id):
+        Document.__init__(self)
+        self.content = content
+        self.id = id
+
+        self.fields_to_tokenize = ["content"]
+
+
+class StopList():
+    def __init__(self, path):
+        with open('./{}'.format(path), 'r') as f:
+            self.stop_list = set(f.read().split('\n'))
+
+    def valid(self, word):
+        return word not in self.stop_list
--- query.py
+++ query.py
@@ -1,6 +1,6 @@
 # coding=utf-8
-import re
-possible_operators = '|&'
+from documents import InvertedIndex
+POSSIBLE_OPERATORS = '|&'
 
 
 class Tree:
@@ -10,123 +10,69 @@ class Tree:
         self.childrens = []
         self.parent = parent
 
-
-def parse(tree, query_string):
-    # regex_word = re.compile(r"/^\w+/")
-    # regex_parenthesis = re.compile(r"/^\(/")
-    # regex_parenthesis_end = re.compile(r"/^\)/")
-
-    # if query_string.match(regex_word):
-    print(query_string)
-    if len(query_string) == 0:
-        return
-    query_string = query_string.replace(' ', '')
-    if query_string[0] not in '()~' + possible_operators:
-        i = 1
-        while i < len(query_string):
-            if query_string[i] in possible_operators + ')':
-                break
-            i += 1
-        if len(tree.childrens) > 0:
-            if i < len(query_string):  # I have an operator
-                if query_string[i] in possible_operators:
-                    node = Tree(tree)
-                    node.childrens.append(tree.childrens[0])
-                    node.operator = tree.operator
-                    node.childrens.append(query_string[:i])
-                    tree.operator = query_string[i]
-                    tree.childrens[0] = node
-                    parse(tree, query_string[i + 1:])
-                elif query_string[i] == ')':
-                    tree.childrens.append(query_string[:i])
-                    parse(tree.parent, query_string[i + 1:])
+    def execute(self, inv_index):
+        #  execute all leaves
+        for i in range(len(self.childrens)):
+            print(self.childrens[i])
+            if isinstance(self.childrens[i], Tree):
+                print("Execute", self.childrens[i])
+                self.childrens[i].execute(inv_index)
+            elif isinstance(self.childrens[i], InvertedIndex):
+                pass
             else:
-                tree.childrens.append(query_string)
-                return
-        else:
-            tree.operator = query_string[i]
-            tree.childrens.append(query_string[:i])
-            parse(tree, query_string[i + 1:])
+                self.childrens[i] = inv_index.filter(r"{}".format(self.childrens[i]))
 
-    if query_string[0] in possible_operators:
-        tree.operator = query_string[0]
-        parse(tree, query_string[1:])
+    @staticmethod
+    def parse(tree, query_string):
+        if len(query_string) == 0:
+            return
+        query_string = query_string.replace(' ', '')
+        if query_string[0] not in '()~' + POSSIBLE_OPERATORS:
+            i = 1
+            while i < len(query_string):
+                if query_string[i] in POSSIBLE_OPERATORS + ')':
+                    break
+                i += 1
+            if len(tree.childrens) > 0:
+                if i < len(query_string):  # I have an operator
+                    if query_string[i] in POSSIBLE_OPERATORS:
+                        node = Tree(tree)
+                        node.childrens.append(tree.childrens[0])
+                        node.operator = tree.operator
+                        node.childrens.append(query_string[:i])
+                        tree.operator = query_string[i]
+                        tree.childrens[0] = node
+                        Tree.parse(tree, query_string[i + 1:])
+                    elif query_string[i] == ')':
+                        tree.childrens.append(query_string[:i])
+                        Tree.parse(tree.parent, query_string[i + 1:])
+                else:
+                    tree.childrens.append(query_string)
+                    return
+            else:
+                tree.operator = query_string[i]
+                tree.childrens.append(query_string[:i])
+                Tree.parse(tree, query_string[i + 1:])
 
-        # find next operator
-        # if len(children) > 0:
-        #   si j'ai un operator dans la string
-        #       je cree un nouveau node
-        #       node.children[0] = tree.children[0]
-        #       node.operator = tree.operator
-        #       node.children[1] = mot trouvé
-        #       node.parent = tree
-        #       tree.operator = operator trouvé
-        #       parse(tree, reste de la string)
-        #   sinon
-        #       je met le mot dans children[1] et basta
-        # else:
-        #   tree.operator = operator found
-        #   node.children[0] = mot trouve
-        #   parse(tree, reste_string)
+        if query_string[0] in POSSIBLE_OPERATORS:
+            tree.operator = query_string[0]
+            Tree.parse(tree, query_string[1:])
 
-    if query_string[0] == '(':
-        node = Tree(tree)
-        tree.childrens.append(node)
-        parse(node, query_string[1:])
-        # create new Node
-        # if len(tree.children) > 0:
-        #   tree.children[1] = node
-        # else
-        #   tree.children[0] = node
-        # parse(node, reste_de_la_string)
+        if query_string[0] == '(':
+            node = Tree(tree)
+            tree.childrens.append(node)
+            Tree.parse(node, query_string[1:])
 
-    if query_string[0] == ')':
-        parse(node.parent, query_string[1:])
-        # parse(node.parent, reste_de_la_string)
+        if query_string[0] == ')':
+            Tree.parse(node.parent, query_string[1:])
 
-    if query_string[0] == '~':
-        node = Tree(tree)
-        if len(tree.childrens) > 0:
-            tree.childrens[1] = node
-        else:
-            tree.childrens[0] = node
-        node.operator = '~'
-        node_child = Tree(node)
-        node.childrens.append(node_child)
-        parse(node_child, query_string[1:])
-
-        # new node
-        # if len(tree.children) > 0:
-        #   tree.children[1] = node
-        # else
-        #   tree.children[0] = node
-        # node.operator = not
-        # new node2
-        # node.children[0] = node2
-        # parse(node2, reste_de_la_string)
-
-
-#     def parse(tree, str):
-# if(str.match(/^\w+/)):
-#     //soit ta string est en mode mot & mat | mut
-#     tree.operator !=
-#     tree.children[1] = tree
-#     tree.parent = new Node()
-
-#     tree.operator = &
-#     tree.children[0] = mot
-#     parse(tree, le reste de la string)
-#     //soit ta string est en mode mot
-#     ok
-# if(str.match(/^\(/))
-#     node = new Node()
-#     tree.children[0] = node
-#     parse(node, reste)
-
-# if(str.match( ')'))
-#     parse(tree.parent, reste)
-
-tree = Tree()
-
-query = "(A | D) & (B | C & E)"
-parse(tree, query)
+        if query_string[0] == '~':
+            node = Tree(tree)
+            if len(tree.childrens) > 0:
+                tree.childrens[1] = node
+            else:
+                tree.childrens[0] = node
+            node.operator = '~'
+            node_child = Tree(node)
+            node.childrens.append(node_child)
+            Tree.parse(node_child, query_string[1:])
