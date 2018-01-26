67e4906950f8759fb5586dfb490af0f2158d8374
JulesDT <jules.denardou@datadoghq.com>
2018-01-26 19:59:29 +0100

    search_model for boolean Model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from documents import *
 from query import Tree
+from search_models import VectorModel, BooleanModel
 
 doc_retreival = {}
 
@@ -8,8 +9,8 @@ stop_list = StopList('common_words')
 tokenizer = DocumentTokenizer(stop_list)
 normalizer = DocumentNormalizer()
 invindex_list = []
-cs_block = CS276Block('./pa1-data/*')
-# cs_block = CASMBlock('cacm.all')
+# cs_block = CS276Block('./pa1-data/*')
+cs_block = CASMBlock('cacm.all')
 for block in cs_block.get_next_block():
     invIndex = InvertedIndex()
     invindex_list.append(invIndex)
@@ -31,12 +32,15 @@ inv_index = invindex_list[0]
 inv_index.build_tf_idf()
 
 vector_model = VectorModel()
+boolean_model = BooleanModel()
 
-doc_ids = inv_index.search("the binary number system offers many advantages", vector_model)
+doc_ids = inv_index.search("Boolean Functions Decimal", boolean_model)
+# doc_ids = inv_index.search("Boolean Decimal", vector_model)
 
+print(doc_ids)
 def pad(str):
     return re.sub( '^',' '*4, str ,flags=re.MULTILINE )
 
-for i, doc_id in enumerate(doc_ids[0:10]):
-    print("Result " + str(i))
+for i, doc_id in enumerate(doc_ids[0:min(len(doc_ids), 10)]):
+    print("Result", str(i), "Document #", doc_id)
     print(pad(doc_retreival[doc_id]))
