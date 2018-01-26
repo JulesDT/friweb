926cc748270580a9a97659da32c525cf5c35af3e
Ghislain Jeanneau <ghislain@messagera.com>
2018-01-26 18:47:56 +0100

    vector model

--- documents.py
+++ documents.py
@@ -1,31 +1,66 @@
 import re
 import glob
 import math
+import collections
+
+class VectorModel:
+    def __init__(self):
+        pass
+
+    def search(self, str, inv_index):
+        str.replace(r"[\n\s]+", " ")
+        tokens = str.split(' ')
+
+        print(tokens)
+
+        query_vector = SparseWordVector()
+        counter = collections.Counter(tokens)
+        for token, amount in counter.items():
+            if token in inv_index.inverted_index:
+                idf = math.log10(len(inv_index.inverted_index) / len(inv_index.inverted_index[token]))
+                query_vector[token] = (1 + math.log10(amount)) * idf
+
+        filtered_tf_idf = {
+            token: inv_index.tf_idf[token]
+            for token in tokens
+            if token in inv_index.tf_idf
+        }
+
+        doc_vectors = collections.defaultdict(SparseWordVector)
+
+        for term, posting in filtered_tf_idf.items():
+            for doc_id, tf_idf in posting.items():
+                doc_vectors[doc_id][term] = tf_idf
+
+        similarities = {doc_id: doc_vector.cosSilimarity(query_vector) for doc_id, doc_vector in doc_vectors.items()}
+        sorted_doc_ids = sorted(similarities, key=lambda k:similarities[k], reverse=True)
+
+        return sorted_doc_ids
 
 class SparseWordVector:
-    def __init__(self, v):
+    def __init__(self, v = {}):
         self.v = v
         self.resetCache()
 
+    def __setitem__(self, k, v):
+        self.v[k] = v
+
     def resetCache(self):
-        self.norm = -1
-        self.cosSilimarity = 2
+        self._norm = -1
 
     def norm(self):
-        if self.norm == -1:
-            self.norm = sum([v * v for k, v in self.v.items()])
-        return self.norm
+        if self._norm == -1:
+            self._norm = sum([v * v for k, v in self.v.items()])
+        return self._norm
 
     def cosSilimarity(self, other):
-        if self.cosSilimarity == 2:
-            v1 = self.v
-            v2 = other.v
-            v1_dims = set(v1.keys)
-            v2_dims = set(v2.keys)
-            common_dims = v1_dims.intersection(v2_dims)
-            num = sum([v1[dim] * v2[dim] for dim in common_dims])
-            self.cosSilimarity = num / (self.norm() * other.norm())
-        return self.cosSilimarity
+        v1 = self.v
+        v2 = other.v
+        v1_dims = set(v1.keys())
+        v2_dims = set(v2.keys())
+        common_dims = v1_dims.intersection(v2_dims)
+        num = sum([v1[dim] * v2[dim] for dim in common_dims])
+        return num / (self.norm() * other.norm())
 
 
 class DocumentTokenizer:
@@ -48,7 +83,7 @@ class DocumentNormalizer:
 
 class InvertedIndex:
     def __init__(self):
-        self.inverted_index = {}
+        self.inverted_index = collections.defaultdict(lambda: collections.defaultdict(int))
         self.base_dict = {}
         self.doc_ids = set()
         self.doc_vectors = {}
@@ -66,10 +101,7 @@ class InvertedIndex:
         return copy
 
     def register(self, token, documentId):
-        if token in self.inverted_index:
-            self.inverted_index[token].add(documentId)
-        else:
-            self.inverted_index[token] = set([documentId])
+        self.inverted_index[token][documentId] += 1
 
     def merge(self, inv_index):
         for token in inv_index.inverted_index.keys():
@@ -79,18 +111,19 @@ class InvertedIndex:
                 self.inverted_index[token] = inv_index.inverted_index[token]
 
     def build_tf_idf(self):
-        idf = {termId: idf_lambda(termInvIdx) for (termId, termInvIdx) in self.inverted_index.items()}
         for (term, termPostings) in self.inverted_index.items():
             idf = math.log10(len(self.inverted_index) / len(termPostings))
             self.tf_idf[term] = {
                 docId: (1 + math.log10(amount))*idf
-                for (docId, amount) in termPostings
+                for (docId, amount) in termPostings.items()
             }
 
     def build_base_vector(self):
         self.base_dict = {k: v for v, k in enumerate(self.inverted_index.keys())}
         print(self.base_dict)
 
+    def search(self, string, model):
+        return model.search(string, self)
 
 class Document:
     def __init__(self):
@@ -147,6 +180,9 @@ class CACMDocument(Document):
 
         self.fields_to_tokenize = ["title", "summary", "keywords"]
 
+    def entry_string(self):
+        return self.title + '\n\n' + self.summary[:500] 
+
     @classmethod
     def from_string(self, document):
         doc_parts = re.split('^\.', document, flags=re.MULTILINE)
@@ -174,6 +210,8 @@ class CS276Document(Document):
 
         self.fields_to_tokenize = ["content"]
 
+    def entry_string(self):
+        return self.content[:500] 
 
 class StopList():
     def __init__(self, path):
