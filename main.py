a2c3f069f879e66d51938c2799fd1a82c3ee76f8
JulesDT <jules.denardou@datadoghq.com>
2018-02-16 19:14:46 +0100

    v5

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import re
 from documents import DocumentNormalizer, DocumentTokenizer, StopList, InvertedIndex, CASMBlock, CS276Block
 from query import Tree
 from search_models import VectorModel, BooleanModel
+import collections
+from functools import reduce
 
 
 stop_list = StopList('common_words')
@@ -10,8 +12,7 @@ tokenizer = DocumentTokenizer(stop_list)
 normalizer = DocumentNormalizer()
 doc_retrieval = {}
 document_data_store = {}
-cs_block = CS276Block('./pa1-data/*')
-# cs_block = CASMBlock('cacm.all')
+# cs_block = CS276Block('./pa1-data/*')
 cs_block = CASMBlock('cacm.all')
 inv_index_list = []
 retrieval_list = []
@@ -29,18 +30,34 @@ for block in cs_block.get_next_block():
 '''
 Map
 '''
-mapped_data = InvertedIndex.map(document_list, tokenizer, normalizer, methods)
+# mapped_data = InvertedIndex.map(document_list, tokenizer, normalizer, methods)
+def map_tokenize(doc):
+    return [
+                (word, doc.id, 1)
+                    for field in doc.fields_to_tokenize
+                        for word in tokenizer.tokenize(getattr(doc, field), normalizer)
+            ]
+mapped_data = map(map_tokenize, document_list)
+mapped_data = [item for sublist in mapped_data for item in sublist]
 '''
 Shuffle
 '''
-shuffled_data = InvertedIndex.shuffle(mapped_data)
+shuffled_data = collections.defaultdict(list)
+for word, doc_id, value in mapped_data:
+    shuffled_data[word].append((doc_id, value))
 '''
 Reduce
 '''
-inv_index = InvertedIndex.reduce(shuffled_data, methods)
+def reducer(reduced_data, new_entry):
+    for entry in new_entry[1]:
+        reduced_data[new_entry[0]][entry[0]] += 1
+    return reduced_data
+inverted_index = reduce(reducer, shuffled_data.items(), collections.defaultdict(lambda: collections.defaultdict(int)))
 '''
 EndMapReduce
 '''
+inv_index = InvertedIndex(methods)
+inv_index.inverted_index = inverted_index
 
 doc_retrieval = retrieval_list[0]
 for doc_retrieval_block in retrieval_list:
@@ -60,3 +77,10 @@ def pad(str):
 for i, doc_id in enumerate(doc_ids[0:min(len(doc_ids), 10)]):
     print("Result", str(i), "Document #", doc_id)
     print(pad(doc_retrieval[doc_id]))
+
+# a = 0
+# for word in inv_index.inverted_index:
+#     a += sum([inv_index.inverted_index[word][doc] for doc in inv_index.inverted_index[word]])
+
+# from IPython import embed
+# embed()
